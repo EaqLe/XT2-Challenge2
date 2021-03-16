0e4c08ec484174000bd34acd723563c2301ecd31
Kevin Kok <hier_kevin@hotmail.com>
2021-03-16 16:51:29 +0100

    Basis neergezet voor opdracht
    Basis neergezet voor opdracht

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+var today = new Date();
+
+// document.getElementById('clock').innerHTML = today.toString(); 
+
+//document.getElementById('clock').innerHTML = today.getDate();
+//document.getElementById('clock').innerHTML = today.getMonth(); // Januari is maand 0
+//document.getElementById('clock').innerHTML = today.getYear(); // Deprecated - niet gebruiken.
+//document.getElementById('clock').innerHTML = today.getFullYear();
+//document.getElementById('clock').innerHTML = today.getDay(); // Zondag is dag 0
+
+//document.getElementById('clock').innerHTML = today.getHours();
+//document.getElementById('clock').innerHTML = today.getMinutes();
+//document.getElementById('clock').innerHTML = today.getSeconds();
+//document.getElementById('clock').innerHTML = today.getMilliseconds();
+
+//document.getElementById('clock').innerHTML = today.getDate() + '/' + (today.getMonth()+1);
+
+//var maanden = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
+//document.getElementById('clock').innerHTML = today.getDate() + ' ' + maanden[today.getMonth()];
+
+
+//document.getElementById('clock').innerHTML = today.getTime();
+

50e118539e1fe8ce0a3a1fb2cc8b6133ee8beddc
Kevin Kok <hier_kevin@hotmail.com>
2021-03-18 00:58:59 +0100

    verder gewerkt aan klok
    klok + datum + animation toegevoegd

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,51 @@
-var today = new Date();
+function klok () {
+	
+	var today = new Date();
+	var hours = today.getHours();
+	var minutes = today.getMinutes();
+	var seconds = today.getSeconds();
+	var months = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
+	var days = new Array('Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag');
 
-// document.getElementById('clock').innerHTML = today.toString(); 
+	//gooit een 0 voor uren kleiner dan 10
+	if (hours < 10){
+		hours = '0' + hours;
+	}
+	//gooit een 0 voor minuten kleiner dan 10
+	if (minutes < 10){
+		minutes = '0' + minutes;
+	}
+	//gooit een 0 voor seconden kleiner dan 10
+	if (seconds < 10){
+		seconds = '0' + seconds;
+	}
 
-//document.getElementById('clock').innerHTML = today.getDate();
+	document.getElementById('hours').innerHTML = hours;
+	document.getElementById('minutes').innerHTML = minutes;
+	document.getElementById('seconds').innerHTML = seconds;
+
+	document.getElementById('month').innerHTML = months[today.getMonth()]; // Januari is maand 0
+	document.getElementById('date').innerHTML = today.getDate();
+	document.getElementById('day').innerHTML = days[today.getDay()]; // Zondag is dag 0
+
+}
+
+klok();
+setInterval(klok, 1000);
+
+
+
+
+
+
 //document.getElementById('clock').innerHTML = today.getMonth(); // Januari is maand 0
 //document.getElementById('clock').innerHTML = today.getYear(); // Deprecated - niet gebruiken.
 //document.getElementById('clock').innerHTML = today.getFullYear();
 //document.getElementById('clock').innerHTML = today.getDay(); // Zondag is dag 0
 
-//document.getElementById('clock').innerHTML = today.getHours();
-//document.getElementById('clock').innerHTML = today.getMinutes();
-//document.getElementById('clock').innerHTML = today.getSeconds();
-//document.getElementById('clock').innerHTML = today.getMilliseconds();
-
 //document.getElementById('clock').innerHTML = today.getDate() + '/' + (today.getMonth()+1);
 
-//var maanden = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
+;
 //document.getElementById('clock').innerHTML = today.getDate() + ' ' + maanden[today.getMonth()];
 
 
